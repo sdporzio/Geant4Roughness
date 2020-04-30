dafe36f5ed07dc878c718c8f37b2949cbe0fb996
Davide S. Porzio <davideporziohep@gmail.com>
2020-04-30 10:55:50 +0100

    minor changes

--- compileAndRun.sh
+++ compileAndRun.sh
@@ -2,7 +2,10 @@ PTFE_NEVENTS=20000
 PTFE_RANDOMSEED=111
 PTFE_VISUAL=0
 
-source /Users/sdporzio/Software/geant4.10.06.p01/install/bin/geant4.sh
+if [ "$GAL" == "/Users/sdporzio/Work/MOUNT/GALILEO" ]
+then
+  source /Users/sdporzio/Software/geant4.10.06.p01/install/bin/geant4.sh
+fi
 cd build
 cmake ../
 make -j 4 && ./main ${PTFE_NEVENTS} ${PTFE_RANDOMSEED} ${PTFE_VISUAL}
--- compileOnly.sh
+++ compileOnly.sh
@@ -1,4 +1,7 @@
-source /Users/sdporzio/Software/geant4.10.06.p01/install/bin/geant4.sh
+if [ "$GAL" == "/Users/sdporzio/Work/MOUNT/GALILEO" ]
+then
+  source /Users/sdporzio/Software/geant4.10.06.p01/install/bin/geant4.sh
+fi
 cd build
 cmake ../
 make -j 4
